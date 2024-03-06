5e90657f16dc06957ada5dc2b9a22302670b1a33
Nikita <nikita021103@mail.ru>
2024-03-06 11:49:12 +0300

    fix

--- src/DropDownMenu/DropDownMenu.js
+++ src/DropDownMenu/DropDownMenu.js
@@ -3,27 +3,19 @@ import s from "./dropDownMenu.module.css"
 import { Checkbox } from '@consta/uikit/Checkbox';
 import {ContextMenu} from '@consta/uikit/ContextMenu'
 
-const DropDownMenu = ({label,name,refs, width}) => {
-    const [value, setValue] = useState([{ label: "DOOSAN 2600LY", active: true, id: 1 }, { label: "DOOSAN 2700LY", active: true, id: 2 },]);
-    const [isOpen, setIsOpen] = useState("");
+const DropDownMenu = ({label, width, value,setValue}) => {
+    const [isOpen, setIsOpen] = useState(false);
     const getItemOnClick = (item) => {
         setValue(prevValue=> prevValue.map(prevItem => prevItem.id === item.id ? {...prevItem, active : !prevItem.active} : prevItem))
     };
-    function renderLeftSide(item) {
-        return <Checkbox size="m" align='center' view='primary' checked={item.active} />;
-    }
   return (
    <div style={{width:width}} className={s.filterButton}>
-       <div className={s.filterLabel} ref={refs} onClick={() => {
-                if (isOpen !== name)
-                    setIsOpen(name)
-                else {
-                    setIsOpen("")
-                }
+       <div className={s.filterLabel} onClick={() => {
+              setIsOpen(!isOpen);
             }}>
                 {label}
             </div>
-    <div className={s.dropDown} style={{display:isOpen === name ?"" : "none"}}>
+    <div className={s.dropDown} style={{display:isOpen === true ?"" : "none"}}>
         {value.map((i)=>{
             return(
                 <div onClick={()=>getItemOnClick(i)} className={s.dropDownItem}><Checkbox checked={i.active} className={s.dropDownCheckbox}/><span>{i.label}</span></div>
--- src/Table/TableConsta.js
+++ src/Table/TableConsta.js
@@ -1,125 +1,143 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { Table } from '@consta/uikit/Table';
-import ProgressBar from '../ProgressBar/ProgressBar';
-import s from "./../TableItem/TableItem.module.css"
-import DropDownMenu from '../DropDownMenu/DropDownMenu';
-
-
+import React, { useEffect, useState, useRef } from "react";
+import { Table } from "@consta/uikit/Table";
+import ProgressBar from "../ProgressBar/ProgressBar";
+import s from "./../TableItem/TableItem.module.css";
+import DropDownMenu from "../DropDownMenu/DropDownMenu";
+import { DatePicker } from "@consta/uikit/DatePicker";
 
 export const TableConsta = () => {
-    const refMachines = useRef(null);
-    const refDate = useRef(null);
-    const refChange = useRef(null);
-    const refTime = useRef(null);
-    const [value, setValue] = useState([{ label: "DOOSAN 2600LY", active: true, id: 1 }, { label: "DOOSAN 2700LY", active: true, id: 2 },]);
+  const [value, setValue] = useState([
+    { label: "DOOSAN 2600LY", active: true, id: 1 },
+    { label: "DOOSAN 2700LY", active: true, id: 2 },
+  ]);
 
-    const [rows, setRows] = useState([
+  const [rows, setRows] = useState([
+    {
+      name: "DOOSAN 2600LY",
+      serial: "NS:152667dl",
+      percent: "45%",
+      loadDay: [
         {
-            name: 'DOOSAN 2600LY',
-            serial: 'NS:152667dl',
-            percent: "45%",
-            loadDay: [{
-                color: "#FF8C00",
-                percent: 10,
-            },
-            { color: "#32CD32", percent: 35 }],
-            programm: "xLk_051",
-            sum: 124230
+          color: "#FF8C00",
+          percent: 10,
         },
+        { color: "#32CD32", percent: 35 },
+      ],
+      programm: "xLk_051",
+      sum: 124230,
+    },
+    {
+      name: "DOOSAN 2700LY",
+      serial: "NS:152667dl",
+      percent: "50%",
+      loadDay: [
         {
-            name: 'DOOSAN 2700LY',
-            serial: 'NS:152667dl',
-            percent: "50%",
-            loadDay: [{
-                color: "#FF8C00",
-                percent: 10,
-            },
-            { color: "#32CD32", percent: 35 }],
-            programm: "xLk_051",
-            sum: 124230
+          color: "#FF8C00",
+          percent: 10,
         },
+        { color: "#32CD32", percent: 35 },
+      ],
+      programm: "xLk_051",
+      sum: 124230,
+    },
+    {
+      name: "DOOSAN 2600LY",
+      serial: "NS:152667dl",
+      percent: "50%",
+      loadDay: [
         {
-            name: 'DOOSAN 2600LY',
-            serial: 'NS:152667dl',
-            percent: "50%",
-            loadDay: [{
-                color: "#FF8C00",
-                percent: 10,
-            },
-            { color: "#32CD32", percent: 35 }],
-            programm: "xLk_051",
-            sum: 124230
+          color: "#FF8C00",
+          percent: 10,
         },
-    ]);
-    const [filteredRows, setFilteredRows] = useState(rows)
-    const columns = [
-        {
-            title: 'Название',
-            accessor: 'name',
-            align: 'left',
-            width: 209.5,
-            sortable: true,
-            renderCell: (row) => (
-                <span style={{ color: "#77CB10" }}>{row.name}</span>
-
-            ),
-        },
-        {
-            title: 'Серийный №',
-            accessor: 'serial',
-            align: "center",
-            sortable: true,
-            width: 209.5,
-        },
-        {
-            title: 'ПРОГРАММА',
-            accessor: 'programm',
-            align: "center",
-            sortable: true,
-            width: 209.5,
-        },
-        {
-            title: 'ЗАГРУЗКА ЗА ТЕКУЩИЙ ДЕНЬ',
-            accessor: 'loadDay',
-            align: "center",
-            sortable: false,
-            renderCell: (row) => (
-                <div className={`${s.progress_cell}`}>  <ProgressBar label={row.loadDay} />
-                    <div className={s.load_time}>
-                        <span>00:00</span>
-                        <span>06:00</span>
-                        <span>12:00</span>
-                        <span>18:00</span>
-                        <span>23:59</span>
-                    </div>
-                </div>
-
-            ),
-            width: 412
-        },
-        {
-            title: 'КОЛ-ВО',
-            accessor: 'sum',
-            align: "center",
-            sortable: true,
-            width: 130
-        },
-        {
-            title: 'ЗАГРУЗКА',
-            accessor: 'percent',
-            align: "center",
-            sortable: true,
-            width: 130
-        },
-    ];
- 
-    return (<div>
-        <div className={s.filterTable}>
-          <DropDownMenu  width={352} refs={refMachines} name={"machines"} label={"Все станки"}/>
-          <DropDownMenu width={98} refs={refDate} name={"date"} label={"Сегодня"}/>
-          <DropDownMenu width={123} refs={refChange} name={"change"} label={"Все смены"}/>
-          <DropDownMenu width={140} refs={refTime} name={"time"} label={"00:00 - 07:59"}/>
+        { color: "#32CD32", percent: 35 },
+      ],
+      programm: "xLk_051",
+      sum: 124230,
+    },
+  ]);
+  const [filteredRows, setFilteredRows] = useState(rows);
+  const columns = [
+    {
+      title: "Название",
+      accessor: "name",
+      align: "left",
+      width: 209.5,
+      sortable: true,
+      renderCell: (row) => <span style={{ color: "#77CB10" }}>{row.name}</span>,
+    },
+    {
+      title: "Серийный №",
+      accessor: "serial",
+      align: "center",
+      sortable: true,
+      width: 209.5,
+    },
+    {
+      title: "ПРОГРАММА",
+      accessor: "programm",
+      align: "center",
+      sortable: true,
+      width: 209.5,
+    },
+    {
+      title: "ЗАГРУЗКА ЗА ТЕКУЩИЙ ДЕНЬ",
+      accessor: "loadDay",
+      align: "center",
+      sortable: false,
+      renderCell: (row) => (
+        <div className={`${s.progress_cell}`}>
+          {" "}
+          <ProgressBar label={row.loadDay} />
+          <div className={s.load_time}>
+            <span>00:00</span>
+            <span>06:00</span>
+            <span>12:00</span>
+            <span>18:00</span>
+            <span>23:59</span>
+          </div>
         </div>
-        <Table zebraStriped='odd' rows={filteredRows} columns={columns} borderBetweenRows={true} borderBetweenColumns={true} />
-    </div>);
+      ),
+      width: 412,
+    },
+    {
+      title: "КОЛ-ВО",
+      accessor: "sum",
+      align: "center",
+      sortable: true,
+      width: 130,
+    },
+    {
+      title: "ЗАГРУЗКА",
+      accessor: "percent",
+      align: "center",
+      sortable: true,
+      width: 130,
+    },
+  ];
+  useEffect(() => {
+    const filteredArray = rows.filter((item1) =>
+      value.some((item2) => item2.label === item1.name && item2.active)
+    );
+    setFilteredRows(filteredArray);
+  }, [value]);
+
+  return (
+    <div>
+      <div className={s.filterTable}>
+        <DropDownMenu
+          value={value}
+          setValue={setValue}
+          width={352}
+          label={"Все станки"}
+        />
+      </div>
+      <Table
+        zebraStriped="odd"
+        rows={filteredRows}
+        columns={columns}
+        borderBetweenRows={true}
+        borderBetweenColumns={true}
+      />
+    </div>
+  );
 };
